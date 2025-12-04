e9619b019dc22814f647956fd3a027247d4168c7
Aru <lexhollowi@gmail.com>
2025-12-04 17:56:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-# main.py
 import asyncio
-from bot import bot
+from bot import bot  # your bot instance
+
+import os
+
+TOKEN = os.environ.get("DISCORD_TOKEN")  # Railway secret or local env variable
 
 async def run_bot():
-    await bot.start(bot.token)
+    await bot.start(TOKEN)
 
 asyncio.run(run_bot())
